6b2eb599c8d1d1570eda4c1bfc2b8049f812ea42
Alison Rodrigues de Souza <alisonsouza@Alisons-MBP.hitronhub.home>
2018-12-05 22:07:45 -0800

    initial commit

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://alison:admin1@ds225294.mlab.com:25294/graphqlmongo');
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('Alisonnnnnn test');
+})
