9991e055485a3b65e1c8742a5c19827759626747
Alison Rodrigues de Souza <alisonsouza@Alisons-MBP.hitronhub.home>
2018-12-06 23:15:52 -0800

    final version

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import {ApolloProvider} from 'react-apollo';
 
 //components
 import BookList from './components/BookList';
+import AddBook from './components/AddBook';
+
 
 //apollo client setup
 const client = new ApolloClient({
@@ -17,6 +19,7 @@ class App extends Component {
       <div id="main">
         <h1>Wherever here</h1>
         <BookList/>
+        <AddBook/>
       </div>
       </ApolloProvider>
     );
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,18 +1,17 @@
-
 import React, { Component } from 'react';
-import {gql} from 'apollo-boost';
 import {graphql} from 'react-apollo';
+import {getBooksQuery} from '../queries/queries';
 
-const getBooksQuery = gql`
-    {
-        books{
-            name
-            id
-        }
-    }
-`
+//components
+import BookDetails from './BookDetails';
 
 class BookList extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            selected: null
+        }
+    }
   displayBooks(){
     var data = this.props.data;
     if(data.loading){
@@ -20,7 +19,7 @@ class BookList extends Component {
     } else {
         return data.books.map(book => {
             return (
-                <li key={book.id}>{book.name}</li>
+                <li key={book.id} onClick={(e) => {this.setState({selected: book.id})}}>{book.name}</li>
             );
         })
     }
@@ -31,6 +30,7 @@ class BookList extends Component {
         <ul id="book-list">
           {this.displayBooks()}
         </ul>
+        <BookDetails bookId={this.state.selected}/>
       </div>
     );
   }
